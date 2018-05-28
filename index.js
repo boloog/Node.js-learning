8cdc5a8c96641600c64bd52c9ca23e5ce461bbc0
boloog <373840427@qq.com>
2018-05-28 22:11:00 +0800

    events 事件操作

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-const greeting = require('./src/greeting')
 
-console.log( greeting.hello() )
+const EventEmitter = require('events')
 
+class Player extends EventEmitter {}
+
+var player = new Player()
+
+// player.on('play', (track) => {
+//   console.log(`正在播放: 《${track}》`)
+// })
+
+// 只执行一次事件 once
+player.once('play', (track) => {
+  console.log(`正在播放: 《${track}》`)
+})
+
+player.emit('play', '光辉岁月')
+player.emit('play', '海阔天空')
+
+
+
+
+
+
+// 倒入模块
+// const greeting = require('./src/greeting')
+
+// console.log( greeting.hello() )
+
+// 引入第三方库
 // const request = require('request')
 
 // request({
