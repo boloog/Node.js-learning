3e105671c2576f6e303c7a62567d7f20c3cc88e2
boloog <373840427@qq.com>
2018-05-28 21:40:23 +0800

    module.exports

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-const request = require('request')
+const greeting = require('./src/greeting')
 
-request({
-  url: 'https://api.douban.com/v2/movie/top250',
-  json: true
-}, (error, response, body) => {
-  console.log(JSON.stringify(body, null, 2))
-})
+console.log( greeting.hello )
+
+// const request = require('request')
+
+// request({
+//   url: 'https://api.douban.com/v2/movie/top250',
+//   json: true
+// }, (error, response, body) => {
+//   console.log(JSON.stringify(body, null, 2))
+// })
